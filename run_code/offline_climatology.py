7797df9677156040a751c96d42e803be0c4b3cfa
Maria Gehne <mgehne@linux1024.psd.esrl.noaa.gov>
2023-02-24 13:33:12 -0700

    convert climatology to K

--- run_code/offline_climatology.py
+++ run_code/offline_climatology.py
@@ -14,6 +14,9 @@ lon = xr.DataArray(vards.lon,dims=('pts'))
 lat = xr.DataArray(vards.lat,dims=('pts'))
 climo = xr.DataArray(vards.climo,dims=('time', 'pts'))
 time = pd.date_range(start='1981-01-01',end='1981-12-31')
+# convert climatology to Kelvin
+climo.values = climo.values + 273.15
+climo.attrs['units'] = 'K'
 # save climatology to netcdf
 dsclim = xr.Dataset({'T2m':climo,'time':time,'lat':lat, 'lon':lon})
 dsclim.to_netcdf('./data_clim/CPC.1981-2010.nc')
