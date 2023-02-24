4c2e0634df906498c2bc3a6cec415f833a3a332c
Maria Gehne <mgehne@linux1024.psd.esrl.noaa.gov>
2023-02-24 11:29:53 -0700

    offline climatology computation for offset

---
+++ run_code/offline_climatology.py
@@ -0,0 +1,19 @@
+import xarray as xr
+import numpy as np
+from lib import driver
+import pandas as pd
+
+
+#  initialize driver
+LIMdriver = driver.Driver(f'namelist_clim.py')
+# read in data
+LIMdriver.get_variables(read=False)
+vards = LIMdriver.use_vars['CPCtemp']['data']
+# get coordinates and variables from varobject
+lon = xr.DataArray(vards.lon,dims=('pts'))
+lat = xr.DataArray(vards.lat,dims=('pts'))
+climo = xr.DataArray(vards.climo,dims=('time', 'pts'))
+time = pd.date_range(start='1981-01-01',end='1981-12-31')
+# save climatology to netcdf
+dsclim = xr.Dataset({'T2m':climo,'time':time,'lat':lat, 'lon':lon})
+dsclim.to_netcdf('./data_clim/CPC.1981-2010.nc')
