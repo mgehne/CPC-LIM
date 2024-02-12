5e4d2647de32e578088f961a6a24f8f87946d761
Yuan-MingC <cheng.ym.cym@gmail.com>
2024-02-12 09:41:56 -0700

    Created V1-1 for linking verification files

---
+++ run_code/V1-1_link_verification_files_JRA.py
@@ -0,0 +1,28 @@
+import os
+# import numpy as np
+# from datetime import datetime as dt,timedelta
+# import netCDF4 as nc
+# from lib.tools import save_ncds
+# import glob
+
+expt_number = 'v2p0'
+in_data_folder = "/Projects/jalbers_process/CPC_LIM/yuan_ming/Data/9b2_sliding_climo_no_double_running_mean"
+out_data_folder = f"/Projects/jalbers_process/CPC_LIM/yuan_ming/Data/{expt_number}_verification"
+
+varnames = ["T2m", "SOIL", "SLP", "colIrr", "H500", "SST", "SF100", "SF750"]
+full_years = list(range(1958, 2024))
+
+print(f'varnames = {varnames}') 
+print("Now link files for the verification period")
+for year in full_years:
+    for varname in varnames:
+        # print(f"---------------- linking {year} for {varname} now ----------------")
+        os.makedirs(os.path.join(out_data_folder, varname), exist_ok=True)
+        source_file = os.path.join(in_data_folder, str(year), varname,f"{varname}.{year}.nc")
+        target_file = os.path.join(out_data_folder, varname, 'links')
+        os.makedirs(target_file, exist_ok=True)
+        
+        if os.path.exists(source_file):
+            os.system(f'ln -sf {source_file} {os.path.join(target_file, f"{varname}.{year}.nc")}')
+        else:
+            print(f"!!!missing {source_file} !!!!!")
