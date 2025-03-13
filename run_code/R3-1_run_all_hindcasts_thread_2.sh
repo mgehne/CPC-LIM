ca95508857aade10cb47bbfac72d7a87439b4802
John Albers <jalbers@linux-compute1.psd.esrl.noaa.gov>
2025-03-13 16:41:26 -0600

    updated retroo runscripts for multithread running

--- run_code/R3-1_run_all_hindcasts_thread_2.sh
+++ run_code/R3-1_run_all_hindcasts_thread_2.sh
@@ -2,7 +2,7 @@
 
 # Loop from 10 down to 1
 # for fold in {10..1}
-for fold in {10..6}
+for fold in {5..1}
 do
     # Run the Python script with the fold number
     # python run_for_v2p0_6_vars_hindcast_fold_${fold}.py
